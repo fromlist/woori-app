e3bdc7fc15be32c025468aa27f28fb67b8a149f9
Kimsungmin <fromlist5@gmail.com>
2024-11-08 16:51:17 +0900

    add variable

--- assets/js/ui-temp.js
+++ assets/js/ui-temp.js
@@ -4,7 +4,7 @@ var current_path = window.location.pathname;
 var localhost = window.location.href;
 
 $(document).ready(function () {
-    vsCode = "vscode://file///C:/kthybrid/"; //source file 경로
+    vsCode = "vscode:///Users/finenuts/Desktop/woori/"; //source file 경로
     var vsCodeHref = '<a class="vscodepath" href=' + '"' + vsCode + current_path + '"></a>';
     $('body').after(vsCodeHref);
 
