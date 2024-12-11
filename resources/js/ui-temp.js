d2dc0c1d79b0ee4a2e53df9c997163b1e9ff23aa
Kimsungmin <fromlist5@gmail.com>
2024-12-11 21:54:45 +0900

    20241211

--- resources/js/ui-temp.js
+++ resources/js/ui-temp.js
@@ -3,15 +3,16 @@ $(document).ready(function () {
         window.location.href = '/html'
         console.log(window.location.href)
     })
-    const scroll = document.querySelector('.table-col.overflow-visible'); // We found the div
-    scroll.addEventListener('scroll', () => {
-        const scrollPosition = scroll.scrollLeft;
-        // We saved the scroll position and check it with console log for any further actions 
-        if (scroll.scrollWidth <= scrollPosition + scroll.offsetWidth) {
-            scroll.classList.add('scroll-end')
-        } else {
-            scroll.classList.remove('scroll-end')
+    const scrollWrap = document.querySelector('.table-col.overflow-visible');
+    if (scrollWrap != null) {
+        scrollWrap.addEventListener('scroll', () => {
+            const scrollPosition = scrollWrap.scrollLeft;
+            if (scrollWrap.scrollWidth <= scrollPosition + scrollWrap.offsetWidth) {
+                scrollWrap.classList.add('scroll-end')
+            } else {
+                scrollWrap.classList.remove('scroll-end')
 
-        }
-    }, false)
+            }
+        }, false)
+    }
 });
